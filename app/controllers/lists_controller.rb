d23c9642e4ca737f557926beb5f5dffbba6f9233
EC2 Default User <ec2-user@ip-172-31-21-110.ap-northeast-1.compute.internal>
2022-09-12 18:17:01 +0000

    [Finish] 3-2

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,39 @@
+class ListsController < ApplicationController
+  def new
+     # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
+    @list = List.new
+  end
+
+  def create
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    # 4. トップ画面へリダイレクト
+    redirect_to '/top'
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+
+
+#他のアクション（index,show,createなど）を巻き込まないよう、
+#privateは一番下。
+#privateより後に定義されたメソッドは、
+#アクションとして認識されなくなり、URLと対応できなくなる。
+private
+# ストロングパラメータ
+  def list_params   #「モデル名_params」
+    params.require(:list).permit(:title, :body)
+#params...formから送られてくるデータはparamsの中に入っています。
+#require...送られてきたデータの中からモデル名(ここでは:list)を指定し、データを絞り込みます。
+#permit...requireで絞り込んだデータの中から、保存を許可するカラムを指定します。
+  end
+end
